aac2b84b988f5e7ad8d946f80f2f6b7fe0fdf962
virtiligo <melangelv@gmail.com>
2018-11-16 01:20:03 +0300

    Added basic structure

---
+++ virgo/app/src/main/java/com/virtiligo/virgovm/VActivity.java
@@ -0,0 +1,10 @@
+package com.virtiligo.virgovm;
+
+public class VActivity extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super(savedInstanceState);
+    }
+}
